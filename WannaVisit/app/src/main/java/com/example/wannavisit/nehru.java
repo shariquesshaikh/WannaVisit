37e8ff21242bfff2d3eb2a8d0451f0c54a3bc4f4
sk-sharique <sharique.shaikh.0123@gmail.com>
2020-12-02 02:39:00 +0530

    Creted the UIs and added basic functionalities

---
+++ WannaVisit/app/src/main/java/com/example/wannavisit/nehru.java
@@ -0,0 +1,13 @@
+package com.example.wannavisit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nehru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nehru);
+    }
+}
