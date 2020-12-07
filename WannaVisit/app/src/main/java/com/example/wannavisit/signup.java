b400f9cc8a88e2d2c511e9035a32f2e9d191e019
sk-sharique <sharique.shaikh.0123@gmail.com>
2020-12-07 16:36:50 +0530

    a minor change

--- WannaVisit/app/src/main/java/com/example/wannavisit/signup.java
+++ WannaVisit/app/src/main/java/com/example/wannavisit/signup.java
@@ -80,8 +80,8 @@ public class signup extends AppCompatActivity {
                             Toast.makeText(signup.this, "Account created successfully.", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),dashboard.class));
 
-                            String msg = "Welcome to WannaVisit. Feel free to explore.";
-                            
+//                            String msg = "Welcome to WannaVisit. Feel free to explore.";
+
                         }else{
                             Toast.makeText(signup.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
