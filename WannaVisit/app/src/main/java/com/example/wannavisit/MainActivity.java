672aa474ae662b1481cb1d7a1396d59a93fb3622
sk-sharique <sharique.shaikh.0123@gmail.com>
2020-12-08 23:37:33 +0530

    SD Card Write permission added, Alpha version tested, Ready for Beta Release

--- WannaVisit/app/src/main/java/com/example/wannavisit/MainActivity.java
+++ WannaVisit/app/src/main/java/com/example/wannavisit/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.wannavisit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     Button mNextBtn;
@@ -21,9 +27,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                File[] Dirs = ContextCompat.getExternalFilesDirs(getApplicationContext(), null);
+
+                File path = Dirs[1];
+
+                path = new File(path + "/wannavisitlog.txt");
+
+                String data = "Welcome to WannaVisit. We are happy to have a user like you. A great product emerges with contribution from the customers like you. We appreciate your effort to use our service. Have a nice day :)";
+
+                try {
+                    File file = new File(String.valueOf(path));
+                    file.createNewFile();
+
+                    FileOutputStream fout = new FileOutputStream(file, true);
+                    fout.write(data.getBytes());
+                    fout.close();
+
+                    Toast.makeText(getBaseContext() , "Welcome!", Toast.LENGTH_LONG ).show();
+
+                } catch (Exception e) {
+                    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
                 startActivity(new Intent(getApplicationContext(), login.class));
             }
         });
 
     }
 }
+
+
+
